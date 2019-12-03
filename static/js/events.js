de804530183cc51b55bc84d8e09d5a60eacf5316
Marian <marian.razvan.oprisan@gmail.com>
2019-12-03 02:14:40 +0200

    Fix UI issue when only title or description were edited

--- static/js/events.js
+++ static/js/events.js
@@ -1,12 +1,18 @@
 // modal task title change
 function modalTitleEdit() {
 	let newTaskTitle = document.querySelector('div.open').firstElementChild.children[0].textContent;
+	let newTaskDescription = document.querySelector('div.open').firstElementChild.children[1].textContent;
 	
 	document.querySelector('div.open').lastElementChild.firstElementChild.children[1].setAttribute('value', newTaskTitle);
+	document.querySelector('div.open').lastElementChild.firstElementChild.children[2].setAttribute('value', newTaskDescription);
 }
 
 function modalDescriptionEdit() {
 	let newTaskDescription = document.querySelector('div.open').firstElementChild.children[1].textContent;
+	let newTaskTitle = document.querySelector('div.open').firstElementChild.children[0].textContent;
 	
 	document.querySelector('div.open').lastElementChild.firstElementChild.children[2].setAttribute('value', newTaskDescription);
+	document.querySelector('div.open').lastElementChild.firstElementChild.children[1].setAttribute('value', newTaskTitle);
 }
+
+// TODO: Merge the functions above is a single function
